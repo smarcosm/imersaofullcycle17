a25b25f8be6dba4aa0c6922857761ed2ce573c01
smarcosm <sebastiaomarcos49@gmail.com>
2024-02-01 12:34:40 -0300

    feat: :sparkles: Criado conexao com database mysql

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+      logging: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
