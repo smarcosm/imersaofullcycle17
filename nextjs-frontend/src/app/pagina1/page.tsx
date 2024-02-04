15eb1f999cfc1a10fa8ededed3cd4a7cdbf0422f
smarcosm <sebastiaomarcos49@gmail.com>
2024-02-04 14:09:22 -0300

    feat: :sparkles: Pagina de listagem de produtos (HOME)

--- nextjs-frontend/src/app/pagina1/page.tsx
+++ nextjs-frontend/src/app/pagina1/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function Pagina1(){
   return (
     <div>
       <h1>Página 1</h1>
+      <Button variant="contained" color="primary" >BBBBB</Button>
     </div>
   )
 }
